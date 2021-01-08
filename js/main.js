1d97c301710a61433e94e0aa2c5801a2221cd74e
Dacemmi2mmi2 <dshevchenko507@gmail.com>
2021-01-08 15:01:52 +0200

    fixed some bugs and added function showWinner

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const htmlElements = {
     field : document.querySelector('.field'),
     fieldElements : document.querySelectorAll('.field>div'),
+    winnerHuman : document.querySelector('.winnerHuman'),
+    winnerCPU : document.querySelector('.winnerCPU'),
 }
 
 
 const variables = {
     countFreeElements : [],
     winnerCoordinates : [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]],
+    check : 0,
 }
 
 
@@ -22,7 +25,6 @@ const playerCPU = function playerComputer(fieldFreeElements)
 
     clickElementCPU.setAttribute('name', 'block_O');
     variables.countFreeElements.splice(variables.countFreeElements.indexOf(fieldFreeElements[position]), 1);
-
     checkWhoWinner();
 }
 
@@ -38,12 +40,20 @@ const checkWhoWinner = function checkingWinner()
             if(htmlElements.fieldElements[element].outerHTML.length === 43 && htmlElements.fieldElements[element].outerHTML.charAt(34) === 'X')
             {
                 countCheck.winnerX ++;
-                countCheck.winnerX === 3 ? console.log('win X') : '';
+                if(countCheck.winnerX === 3)
+                {
+                    variables.check ++;
+                    winner('Human');
+                }
             }
             if(htmlElements.fieldElements[element].outerHTML.length === 43 && htmlElements.fieldElements[element].outerHTML.charAt(34) === 'O')
             {
                 countCheck.winnerO ++;
-                countCheck.winnerO === 3 ? console.log('win O') : '';
+                if(countCheck.winnerO === 3)
+                {
+                    variables.check ++;
+                    winner('CPU');
+                }
             }
         });
     });
@@ -57,12 +67,18 @@ htmlElements.field.addEventListener('click', (event) => {
         if(!clickElement.hasAttribute('name'))
         {
             clickElement.setAttribute('name', 'block_X');
-            if(variables.countFreeElements.length != 1)
+            if(variables.countFreeElements.length != 0)
             {
                 variables.countFreeElements.splice(variables.countFreeElements.indexOf(event.target.className), 1);
                 checkWhoWinner();
-                setTimeout(() => {playerCPU(variables.countFreeElements)}, 750);
+                variables.check === 0 ? setTimeout(() => {playerCPU(variables.countFreeElements)}, 100) : '';
             }
         }
     }
 });
+
+
+const winner = function showWinner(nameWinner)
+{
+    nameWinner === 'Human' ? htmlElements.winnerHuman.style.left = 'auto' : htmlElements.winnerCPU.style.left = 'auto';
+}
